efd1c820614e71f8b2ec473827af4115ff477fed
Xu109 <1121134481@qq.com>
2020-06-29 01:56:03 +0800

    添加了二分查找和指针的总结

--- 6.25/6.25思考题.py
+++ 6.25/6.25思考题.py
@@ -1,7 +1,7 @@
 """
 Author:  Mr.Xu
-Create:  2020/6/25 8:17
-Github:  https://github.com/WeaponMaster
+Create:  2020/6/29 1:17
+Github:  https://github.com/Xu109
 Copyright (c) 2020, Mr.Xu Group All Rights Reserved.
 """
 '''分离指针专题
@@ -25,31 +25,25 @@ Copyright (c) 2020, Mr.Xu Group All Rights Reserved.
 
 
 
-def intersect(nums1, nums2):
-    result = []
+from typing import List
+def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
     nums1.sort()
     nums2.sort()
     i = 0
     j = 0
+    num_set = set()
     while i < len(nums1) and j < len(nums2):
-        if i > 0 and nums1[i] == nums1[i - 1]:
-            i += 1
-            continue
         if nums1[i] == nums2[j]:
-            result.append(nums1[i])
+            num_set.add(nums1[i])
             i += 1
             j += 1
         elif nums1[i] < nums2[j]:
             i += 1
         else:
             j += 1
-    return result
+    return list(num_set)
 
 
-if __name__ == '__main__':
-    a = [1, 2, 2, 1]
-    b = [2, 2]
-    print(intersect(a, b))
 '''————————————————
 版权声明：本文为CSDN博主「愉悦的麻婆豆腐」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
 原文链接：https://blog.csdn.net/u010788049/java/article/details/81437834
@@ -73,11 +67,11 @@ nums2 = [2,5,6],       n = 3
 
 
 def merge(nums1, m, nums2, n):
-    i = m - 1  # i=2
-    j = n - 1  # j=2
+    i = m - 1
+    j = n - 1
     k = m + n - 1
     while i >= 0 and j >= 0:
-        if nums1[i] >= nums2[j]:  # 把大的数放到nums1的最后一位，倒着放入
+        if nums1[i] >= nums2[j]:
             nums1[k] = nums1[i]
             i -= 1
         else:
@@ -93,3 +87,71 @@ def merge(nums1, m, nums2, n):
         k -= 1
         j -= 1
     return nums1
+
+# 二分查找
+
+
+def binarySearch(nums: List, val: int):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        if val not in nums:
+            return -1
+        if val == nums[left]:
+            return left
+        elif val == nums[right]:
+            return right
+        else:
+            mid = (left + right) // 2
+            if val < nums[mid]:
+                right = mid
+            elif val > nums[mid]:
+                left = mid
+            else:
+                return mid
+
+
+def binarySearch2(nums: List, val: int):
+    left = -1
+    right = len(nums)
+    while left <= right:
+        if val not in nums:
+            return -1
+        mid = (left + right) // 2
+        if val < nums[mid]:
+            right = mid
+        elif val > nums[mid]:
+            left = mid
+        else:
+            return mid
+
+
+def binarySearch3(nums: List, val: int):
+    left = -1
+    right = len(nums)
+    while left <= right:
+        mid = left + (right - left) // 2
+        if val < nums[mid]:
+            right = mid
+        elif val > nums[mid]:
+            left = mid
+        else:
+            return mid
+
+
+def binarySearch4(nums: List, val: int, left: int, right: int):
+    if right > 0:
+        mid = left + (right - left) // 2
+        if nums[mid] == val:
+            return mid
+        elif nums[mid] > val:
+            return binarySearch4(nums, val, left, mid - 1)
+        else:
+            return binarySearch4(nums, val, mid + 1, right)
+    else:
+        return -1
+
+
+if __name__ == '__main__':
+    a = list(range(20))
+    print(binarySearch4(a, 9, 0, 19))
