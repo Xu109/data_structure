9891922bb34de364d6522395523221b1a2efa287
Xu109 <1121134481@qq.com>
2020-06-29 17:40:33 +0800

    添加了最长公共前缀

---
+++ 6.29/最长公共前缀.py
@@ -0,0 +1,68 @@
+"""
+Author:  Mr.Xu
+Create:  2020/6/29 15:01
+Github:  https://github.com/Xu109
+Copyright (c) 2020, Mr.Xu Group All Rights Reserved.
+"""
+
+"""
+14 编写一个函数来查找字符串数组中的最长公共前缀。
+链接：https://leetcode-cn.com/problems/longest-common-prefix
+
+示例 1:
+输入: ["flower","flow","flight"]
+输出: "fl"
+
+示例 2:
+输入: ["dog","racecar","car"]
+输出: ""
+解释: 输入不存在公共前缀。
+
+"""
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+zipped = list(zip(a, b))
+print(zipped)
+for each in zipped:
+    print(each)
+
+for each in zip(*zipped):
+    print(each)
+
+strs = ["flower", "flow", "flight"]
+minLength = min([len(s) for s in strs])
+print(minLength)
+
+from typing import List
+
+# def longestCommonPrefix(strs: List[str]):
+#     if len(strs) == 0 or "" in strs:
+#         return ""
+#     if len(strs) == 1:
+#         return strs
+#     minLength = min([len(s) for s in strs])
+#     publicWord = []
+#     for i in range(minLength):
+#         for word in strs:
+#             publicWord.append(word[:i+1])
+#         if len(set(publicWord)) == 1:
+#             publicWord = []
+#         else:
+#             return strs[0][:i]
+#
+# ["flower", "flow", "flight"]
+def longCommonPrefix(strs: List[str]):
+    res = ''
+    for each in zip(*strs):
+        if len(set(each)) == 1:
+            res += each[0]
+        else:
+            return res
+    return res
+#
+#
+# if __name__ == '__main__':
+#     l = ["flower", "flow", "flight"]
+#     print(longCommonPrefix(l))
+#     print(longestCommonPrefix(l))
